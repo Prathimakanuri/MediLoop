2d71c29dc7ebe58c7ac83630843e83c2ed3ff560
Kanuri Naga Prathima Sri <prathimakanuri2006@gmail.com>
2026-09-04 00:01:46 +0530

    Fix production module imports

--- src/app/bookings/[id]/page.tsx
+++ src/app/bookings/[id]/page.tsx
@@ -7,9 +7,9 @@ import { Header } from '../../../components/common/Header';
 import { MobileNav } from '@/components/common/MobileNav';
 import { Footer } from '@/components/common/Footer';
 import { EquipmentImage } from '@/components/common/EquipmentImage';
-import { PaymentModal } from '@/components/bookings/PaymentModal';
+import { PaymentModal } from '../../../components/bookings/PaymentModal';
 import { Booking, User } from '@/types';
-import { formatDate, formatCurrency, getStatusColor } from '@/lib/utils';
+import { formatDate, formatCurrency, getStatusColor } from '../../../lib/utils';
 import {
   ArrowLeft,
   Building2,
